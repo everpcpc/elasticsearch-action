15283e2f54f7a568dd795c9ab691d55caf4ba3a2
everpcpc <git@everpcpc.com>
2020-03-24 10:03:23 +0800

    only single

--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -18,42 +18,18 @@ docker run --rm \
   docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
   -c -C /usr/share/elasticsearch/ config | tar x -C /usr/share/elasticsearch/
 
+chown -R 1000:1000 /usr/share/elasticsearch/plugins/
 
 if [[ ! -z $PLUGINS ]]; then
   docker run --rm \
     --network=elastic \
+    --use=1000 \
     -v /usr/share/elasticsearch/plugins/:/usr/share/elasticsearch/plugins/ \
     -v /usr/share/elasticsearch/config/:/usr/share/elasticsearch/config/ \
     --entrypoint=/usr/share/elasticsearch/bin/elasticsearch-plugin \
     docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
     install ${PLUGINS/\\n/ } --batch
 fi
-
-NODES=${NODES-1}
-for (( node=1; node<=$NODES; node++ ))
-do
-  port=$((9200 + $node - 1))
-  docker run \
-    --rm \
-    --env "node.name=es${node}" \
-    --env "cluster.name=docker-elasticsearch" \
-    --env "cluster.initial_master_nodes=es1" \
-    --env "discovery.seed_hosts=es1" \
-    --env "cluster.routing.allocation.disk.threshold_enabled=false" \
-    --env "bootstrap.memory_lock=true" \
-    --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
-    --env "xpack.security.enabled=false" \
-    --env "xpack.license.self_generated.type=basic" \
-    --ulimit nofile=65536:65536 \
-    --ulimit memlock=-1:-1 \
-    --publish "${port}:9200" \
-    --detach \
-    --network=elastic \
-    --name="es${node}" \
-    -v /usr/share/elasticsearch/plugins/:/usr/share/elasticsearch/plugins/:ro \
-    -v /usr/share/elasticsearch/config/:/usr/share/elasticsearch/config/:ro \
-    docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
-done
 
 docker run \
   --network elastic \
